6d7d9a2b5a0bbda9cf7893a9a64b4446c8e1dc1c
Laura Antunez <lauraantunez024@gmail.com>
2024-05-18 02:48:10 -0400

    last one

--- src/app/components/SkillBlock.jsx
+++ src/app/components/SkillBlock.jsx
@@ -2,7 +2,7 @@ export default function SkillBlock({ type, skillSet }) {
   return (
     
       <div class=''>
-        <h1 class='text-3xl flex flex-col text-left font-medium pl-12'> {type} </h1>
+        <h1 class='text-3xl flex flex-col text-left font-medium pl-12' id='skill-type'> {type} </h1>
         <div class='flex flex-row justify-center skill-card'>
           {skillSet.map((skill, index) => (
             <div class="secondary-background skill-shape my-5 mx-4 animation">
